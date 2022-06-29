bd247df27c80935db09035fc531651a30a412378
Marina Portugal <marinapsvreis@gmail.com>
2022-06-29 01:51:35 -0300

    api model para atividade 1

---
+++ src/main/java/com/residencia/comercio/controllers/AutenticacaoController.java
@@ -0,0 +1,42 @@
+package com.residencia.comercio.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.residencia.comercio.dtos.TokenDTO;
+import com.residencia.comercio.entities.Usuario;
+import com.residencia.comercio.services.TokenService;
+
+@RestController
+@RequestMapping("/autenticacao")
+public class AutenticacaoController {
+	@Autowired
+	AuthenticationManager authenticationManager;
+	
+	@Autowired
+	TokenService tokenService;
+	
+	@PostMapping
+	public ResponseEntity<TokenDTO> auth(@RequestBody Usuario usuario){
+		
+		UsernamePasswordAuthenticationToken 
+			usernamePasswordAuthenticationToken = 
+				new UsernamePasswordAuthenticationToken(usuario.getEmail(), usuario.getSenha());
+		
+		Authentication 
+			authentication = 
+			authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+		
+		String token = tokenService.generateToken(authentication);
+		TokenDTO tokenDTO = new TokenDTO("Bearer", token);
+		return new ResponseEntity<>(tokenDTO, HttpStatus.OK);
+	}
+}
